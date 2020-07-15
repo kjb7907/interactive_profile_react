2a91992c058917551df850e4f55ffcf7806e4ece
김정빈(개인) <kjb7907@naver.com>
2020-07-16 01:24:16 +0900

    이벤트 핸들링 및 캐릭터 액션 확인

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,13 +2,55 @@ import React, { Component } from 'react';
 import 'assets/styles/character.scss'
 
 class Character extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      character: 'charizard',
+      form: 'normal',
+      direction: 'right',
+    }
+  }
+
+  changeAction = (form,direction)=>{
+    this.setState({
+      form: form,
+      direction: direction
+    })
+  }
+
+  componentWillReceiveProps(nextProps){
+    // 부모에게서 받은 props 가 변경되었을때의 라이프 사이클 훅
+    let {action} = nextProps
+    switch (action) {
+      case 'd':
+        this.changeAction('normal', 'right')
+        break;
+      case 'r':
+        this.changeAction('run', 'right')
+        break;
+      case 'l':
+        this.changeAction('run', 'left')
+        break;
+      default:
+        break;
+    }
+  }
+
   render() {
+    let className = `character ${this.state.character} ${this.state.form} ${this.state.direction}`
+
     return (
       <div className="character-box">
-      
+        <div className={className}>
+        </div>
       </div>
     );
   }
 }
+
+Character.defaultProps = {
+  action: 'default'
+};
 
 export default Character;
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,46 @@ import 'assets/styles/profile.scss'
 import Character from 'components/Character'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      action: 'd' // d: 정지 r: 오른쪽으로 가기 l: 왼쪽으로 가기
+    }
+  }
+
+  handleDefault = ()=> {
+    this.setState({
+      action: 'd'
+    })
+  }  
+
+  handleRight = ()=> {
+    this.setState({
+      action: 'r'
+    })
+  }
+
+  handleLeft = ()=> {
+    this.setState({
+      action: 'l'
+    })
+  }  
+
   render() {
     return (
       <div className="profile-app">
-        <Character/>
+        <div className="app-handler">
+          <div className="left" 
+            onMouseDown={this.handleLeft} 
+            onMouseUp={this.handleDefault}>
+          </div>
+          <div className="right" 
+            onMouseDown={this.handleRight} 
+            onMouseUp={this.handleDefault}>
+          </div>
+        </div>
+        <Character action={this.state.action}/>
       </div>
     );
   }
